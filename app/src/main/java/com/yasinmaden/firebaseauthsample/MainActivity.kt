4ec0da9fd35a6c51c0a736100304e674e4f71091
Yasin Maden <maden.yasin@hotmail.com>
2024-08-21 23:40:04 +0300

    Splash Screen done.

--- app/src/main/java/com/yasinmaden/firebaseauthsample/MainActivity.kt
+++ app/src/main/java/com/yasinmaden/firebaseauthsample/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.yasinmaden.firebaseauthsample
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.yasinmaden.firebaseauthsample.navigation.NavGraph
+import com.yasinmaden.firebaseauthsample.ui.splash.SplashViewModel
 import com.yasinmaden.firebaseauthsample.ui.theme.FirebaseAuthSampleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val splashViewModel: SplashViewModel by viewModels()
+
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        // Keep the splash screen visible until the condition is met
+        splashScreen.setKeepOnScreenCondition {
+            splashViewModel.isSplashShow.value
+        }
         enableEdgeToEdge()
         setContent {
             FirebaseAuthSampleTheme {
