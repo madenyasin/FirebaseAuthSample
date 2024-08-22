518724fceb684d6bf16062a5b7e744d1505afdc5
Yasin Maden <maden.yasin@hotmail.com>
2024-08-22 14:59:50 +0300

    Sign Up Navigation

--- app/src/main/java/com/yasinmaden/firebaseauthsample/navigation/NavGraph.kt
+++ app/src/main/java/com/yasinmaden/firebaseauthsample/navigation/NavGraph.kt
@@ -7,9 +7,12 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.yasinmaden.firebaseauthsample.ui.login.LoginContract
 import com.yasinmaden.firebaseauthsample.ui.main.MainScreen
 import com.yasinmaden.firebaseauthsample.ui.login.LoginScreen
 import com.yasinmaden.firebaseauthsample.ui.login.LoginViewModel
+import com.yasinmaden.firebaseauthsample.ui.sign_up.SignUpScreen
+import com.yasinmaden.firebaseauthsample.ui.sign_up.SignUpViewModel
 
 @Composable
 fun NavGraph(navController: NavHostController) {
@@ -23,12 +26,25 @@ fun NavGraph(navController: NavHostController) {
                 uiState = uiState,
                 uiEffect = uiEffect,
                 onAction = viewModel::onAction,
-                onNavigateMainScreen = { navController.navigate("main") }
+                onNavigateMainScreen = { navController.navigate("main") },
+                onNavigateSignUpScreen = {navController.navigate("sign_up")}
             )
         }
 
         composable("main") {
             MainScreen()
+        }
+
+        composable("sign_up") {
+            val viewModel: SignUpViewModel = hiltViewModel()
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+            val uiEffect = viewModel.uiEffect
+            SignUpScreen(
+                uiState = uiState,
+                uiEffect = uiEffect,
+                onAction = viewModel::onAction,
+                onNavigateMainScreen = { navController.navigate("main") },
+            )
         }
 
 
