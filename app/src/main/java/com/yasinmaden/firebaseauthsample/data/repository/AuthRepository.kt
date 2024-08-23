fac1f5aa5bfbcf81adb8fb54cf0ca1b46c6af070
Yasin Maden <maden.yasin@hotmail.com>
2024-08-23 10:49:35 +0300

    getCurrentUserData ve signOut fonksiyonları eklendi

--- app/src/main/java/com/yasinmaden/firebaseauthsample/data/repository/AuthRepository.kt
+++ app/src/main/java/com/yasinmaden/firebaseauthsample/data/repository/AuthRepository.kt
@@ -2,10 +2,12 @@ package com.yasinmaden.firebaseauthsample.data.repository
 
 import com.google.firebase.auth.FirebaseAuth
 import com.yasinmaden.firebaseauthsample.common.Resource
+import com.yasinmaden.firebaseauthsample.data.UserData
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class AuthRepository @Inject constructor(private val auth: FirebaseAuth) {
+    fun isUserLoggedIn(): Boolean = auth.currentUser != null
 
     suspend fun signUp(email: String, password: String): Resource<String> {
         return try {
@@ -25,5 +27,31 @@ class AuthRepository @Inject constructor(private val auth: FirebaseAuth) {
         }
     }
 
-    fun isUserLoggedIn(): Boolean = auth.currentUser != null
+    fun getCurrentUserData(): Resource<UserData> {
+        return try {
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                val userData = UserData(
+                    name = currentUser.displayName.orEmpty(),
+                    email = currentUser.email.orEmpty(),
+                    profilePicUrl = currentUser.photoUrl?.toString().orEmpty()
+                )
+                Resource.Success(userData)
+            } else {
+                Resource.Error(Exception("User not logged in"))
+            }
+        } catch (e: Exception) {
+            Resource.Error(e)
+        }
+    }
+
+    fun signOut(): Resource<Unit> {
+        return try {
+            auth.signOut()
+            Resource.Success(Unit)
+        } catch (e: Exception) {
+            Resource.Error(e)
+        }
+    }
+
 }
